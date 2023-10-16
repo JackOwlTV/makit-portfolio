ec6f12a188c8de2554471ba88c8885b5614a20a4
JackOwlTV <dylan.vivant@sfr.fr>
2023-10-16 20:32:58 +0200

    Starting Landing page
    Dans ce premier commit, j'ai créer la page d'accueil, a l'ouverture du site c'est cette page qui sera présente, j'y ai ajouté le dark mode.
    
    Dans le prochain commit j'améliorerai la transition entre le dark mode.

---
+++ app/components/Landing Header/LandingHeader.jsx
@@ -0,0 +1,30 @@
+'use client'
+import React, { useState } from 'react';
+import './LandingHeader.css';
+import { MdEmojiObjects } from "react-icons/md";
+
+const LandingHeader = () => {
+    const [isDarkMode, setDarkMode] = useState(false);
+
+    const toggleDarkMode = () => {
+        setDarkMode(!isDarkMode);
+    }
+
+    const headerClass = `landing-header ${isDarkMode ? 'dk-mode' : ''}`;
+    const emojiClass = `emoji ${isDarkMode ? 'dark' : 'light-mode'}`;
+    const titleMajClass = `${isDarkMode ? 'dark-maj' : 'title-maj'}`;
+    const titleCursClass = `${isDarkMode ? 'dark-curs' : 'title-curs'}`;
+
+
+    return (
+        <section className={headerClass}>
+            <MdEmojiObjects className={emojiClass} onClick={toggleDarkMode} />
+            <div className="title-project">
+                <div className={titleMajClass}>MAKE IT</div>
+                <div className={titleCursClass}>differently</div>
+            </div>
+        </section>
+    );
+}
+
+export default LandingHeader;
