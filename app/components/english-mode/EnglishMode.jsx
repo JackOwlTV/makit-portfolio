7b3daed1c054f12091cad0dd83cc232d2599436c
JackOwlTV <dylan.vivant@sfr.fr>
2023-11-14 14:58:47 +0100

    Structure

--- app/components/english-mode/EnglishMode.jsx
+++ app/components/english-mode/EnglishMode.jsx
@@ -1,22 +1,21 @@
-import React from 'react'
-import Link from 'next/link'
-import "./EnglishMode.css"
+import React from "react";
+import Link from "next/link";
+import "./EnglishMode.css";
 
-import { kelson_regular } from '@/app/fonts/kelson/kelson'
-
+import { kelson_regular } from "@/app/fonts/kelson/kelson";
 
 function EnglishMode(props) {
-    return (
-        <div className={props.langues}>
-            <Link href={"#"}>
-                <div className={props.rect} />
-                <div className={kelson_regular.className}>
-                <div className={props.en} >en</div>
-                <div className={props.fr}>fr</div>
-                </div>
-            </Link>
+  return (
+    <div className={props.langues}>
+      <Link href={"#"}>
+        <div className={props.rect} />
+        <div className={kelson_regular.className}>
+          <div className={props.en}>en</div>
+          <div className={props.fr}>fr</div>
         </div>
-    )
+      </Link>
+    </div>
+  );
 }
 
-export default EnglishMode
+export default EnglishMode;
--- app/page.js
+++ app/page.js
@@ -1,30 +1,35 @@
-'use client'
-import React, { useState, useEffect, useRef } from 'react';
-import 'animate.css';
+"use client";
+import React, { useState, useEffect, useRef } from "react";
+import "animate.css";
 
-import LandingHeader from "./components/Landing Header/LandingHeader.jsx"
-import Cadre from "./components/Cadre/Cadre.jsx"
+import LandingHeader from "./components/Landing Header/LandingHeader.jsx";
+import Cadre from "./components/Cadre/Cadre.jsx";
 
-
-import './components/Cadre/Cadre.css'
-import './components/Landing Header/LandingHeader.css'
+import "./components/Cadre/Cadre.css";
+import "./components/Landing Header/LandingHeader.css";
 
 export default function Home() {
-
   const [isDarkMode, setDarkMode] = useState(false);
 
   const toggleDarkMode = () => {
     setDarkMode(!isDarkMode);
-  }
-  
-  const mainClass = `${isDarkMode ? 'main-dk min-h-screen' : ' main min-h-screen'}`;
-  const pgProjectsClass = `${isDarkMode ? 'pg-projects-dk' : ' pg-projects'}`;
-  const rectangle2Class = `${isDarkMode ? 'rectangle2 rect-dk' : 'rectangle2'}`;
-  const titleMajClass = `${isDarkMode ? 'lp-title-maj maj-dk' : 'lp-title-maj'}`;
-  const titleCursClass = `${isDarkMode ? 'lp-title-curs curs-dk' : 'lp-title-curs'}`;
-  const headerClass = `${isDarkMode ? 'dk-mode' : ' landing-header animate__fadeIn'}`;
-  const emojiClass = `emoji ${isDarkMode ? 'dark' : 'light-mode'}`;
+  };
 
+  const mainClass = `${
+    isDarkMode ? "main-dk min-h-screen" : " main min-h-screen"
+  }`;
+  const pgProjectsClass = `${isDarkMode ? "pg-projects-dk" : " pg-projects"}`;
+  const rectangle2Class = `${isDarkMode ? "rectangle2 rect-dk" : "rectangle2"}`;
+  const titleMajClass = `${
+    isDarkMode ? "lp-title-maj maj-dk" : "lp-title-maj"
+  }`;
+  const titleCursClass = `${
+    isDarkMode ? "lp-title-curs curs-dk" : "lp-title-curs"
+  }`;
+  const headerClass = `${
+    isDarkMode ? "dk-mode" : " landing-header animate__fadeIn"
+  }`;
+  const emojiClass = `emoji ${isDarkMode ? "dark" : "light-mode"}`;
 
   const section1Ref = useRef();
   const section2Ref = useRef();
@@ -32,25 +37,27 @@ export default function Home() {
   useEffect(() => {
     const observerOptions = {
       root: null,
-      rootMargin: '0px',
+      rootMargin: "0px",
       threshold: 0.5, // Lorsque 50 % de la section est visible
     };
 
     const handleIntersection = (entries, observer) => {
       entries.forEach((entry) => {
-        const sectionDiv = entry.target.querySelector('.observe-me'); // Classe personnalisée
+        const sectionDiv = entry.target.querySelector(".observe-me"); // Classe personnalisée
         if (entry.isIntersecting && sectionDiv) {
-          sectionDiv.classList.add('show');
-          sectionDiv.classList.remove('hidden');
+          sectionDiv.classList.add("show");
+          sectionDiv.classList.remove("hidden");
         } else if (sectionDiv) {
-          sectionDiv.classList.remove('show');
-          sectionDiv.classList.add('hidden');
-
+          sectionDiv.classList.remove("show");
+          sectionDiv.classList.add("hidden");
         }
       });
     };
 
-    const observer = new IntersectionObserver(handleIntersection, observerOptions);
+    const observer = new IntersectionObserver(
+      handleIntersection,
+      observerOptions
+    );
 
     if (section1Ref.current) {
       observer.observe(section1Ref.current);
@@ -67,41 +74,41 @@ export default function Home() {
 
   return (
     <main className={mainClass}>
-
-      <section ref={section1Ref} className={'slider'}>
-        <div className='observe-me hidden'>
+      <section ref={section1Ref} className={"slider"}>
+        <div className="observe-me hidden">
           <LandingHeader
             click={toggleDarkMode}
             emojiClass={emojiClass}
             headerClass={headerClass}
-            titleMajClass={isDarkMode ? 'dark-maj' : 'title-maj'}
-            titleCursClass={isDarkMode ? 'dark-curs' : 'title-curs'}
-          /></div>
+            titleMajClass={isDarkMode ? "dark-maj" : "title-maj"}
+            titleCursClass={isDarkMode ? "dark-curs" : "title-curs"}
+          />
+        </div>
       </section>
 
-      <section id={"project"} ref={section2Ref} className={'slider'}>
-        <div className='observe-me hidden'>
-          <Cadre cadreClass={pgProjectsClass}
+      <section id={"project"} ref={section2Ref} className={"slider"}>
+        <div className="observe-me hidden">
+          <Cadre
+            cadreClass={pgProjectsClass}
             click={toggleDarkMode}
             rect2Class={rectangle2Class}
             majClass={titleMajClass}
             cursClass={titleCursClass}
-            rectmenuClass={isDarkMode ? 'rectangle4 rect-dk' : 'rectangle4'}
-            vectorClass={isDarkMode ? 'Vector-dk Vector' : 'Vector'}
-            dkClass='darkmode'
-            dkRectClass={isDarkMode ? 'rectangle-dk rect-dk' : 'rectangle-dk'}
-            dkIconClass={isDarkMode ? 'icon icon-dk' : 'icon'}
-            rect5Class={isDarkMode ? 'rectangle5 rect-dk' : 'rectangle5'}
-            languesClass='englishmode'
-            enClass={isDarkMode ? 'en en-dk' : 'en'}
-            frClass={isDarkMode ? 'fr fr-dk' : 'fr'}
-            projects={isDarkMode ? 'project-dk' : 'project'}
-            project={isDarkMode ? 'lg-project-dk' : 'lg-project'}
-            title={isDarkMode ? 'title-dk' : 'title-projects'}
+            rectmenuClass={isDarkMode ? "rectangle4 rect-dk" : "rectangle4"}
+            vectorClass={isDarkMode ? "Vector-dk Vector" : "Vector"}
+            dkClass="darkmode"
+            dkRectClass={isDarkMode ? "rectangle-dk rect-dk" : "rectangle-dk"}
+            dkIconClass={isDarkMode ? "icon icon-dk" : "icon"}
+            rect5Class={isDarkMode ? "rectangle5 rect-dk" : "rectangle5"}
+            languesClass="englishmode"
+            enClass={isDarkMode ? "en en-dk" : "en"}
+            frClass={isDarkMode ? "fr fr-dk" : "fr"}
+            projects={isDarkMode ? "project-dk" : "project"}
+            project={isDarkMode ? "lg-project-dk" : "lg-project"}
+            title={isDarkMode ? "title-dk" : "title-projects"}
           />
-
         </div>
       </section>
     </main>
-  )
+  );
 }
