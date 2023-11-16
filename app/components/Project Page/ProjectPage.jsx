7dc2a8da79f637f04090cfc31a328785fb2b5f15
JackOwlTV <dylan.vivant@sfr.fr>
2023-11-16 13:57:55 +0100

    Page error ajout

--- app/components/Project Page/ProjectPage.jsx
+++ app/components/Project Page/ProjectPage.jsx
@@ -1,65 +1,60 @@
-'use client'
-import React, { useState } from 'react'
-import { Projects } from '../../data/Projects'
-import { kelson_regular, kelson_meduim } from '@/app/fonts/kelson/kelson'
+"use client";
+import React, { useState } from "react";
+import { Projects } from "../../data/Projects";
+import { kelson_regular, kelson_meduim } from "@/app/fonts/kelson/kelson";
 
-import ProjectLigne from './Projet Ligne/ProjectLigne'
-import ImgProject from '../ImgProject/ImgProject'; // Importez le composant ImgProject
-import './ProjectPages.css'
-import './Projet Ligne/ProjectLigne.css'
-import 'animate.css';
-
-
+import ProjectLigne from "./Projet Ligne/ProjectLigne";
+import ImgProject from "../ImgProject/ImgProject"; // Importez le composant ImgProject
+import "./ProjectPages.css";
+import "./Projet Ligne/ProjectLigne.css";
+import "animate.css";
 
 const ProjectPage = (props) => {
-    const [hoveredProject, setHoveredProject] = useState(null);
+  const [hoveredProject, setHoveredProject] = useState(null);
 
-    const handleMouseEnter = (project) => {
-        setHoveredProject(project);
-    };
+  const handleMouseEnter = (project) => {
+    setHoveredProject(project);
+  };
 
-    const handleMouseLeave = () => {
-        setHoveredProject(null);
-    };
-    return (
+  const handleMouseLeave = () => {
+    setHoveredProject(null);
+  };
+  return (
+    <div className={props.projects}>
+      <div className="gp-mask">
+        <div className="bg-mask" />
+        {hoveredProject && (
+          <div className="project-cover">
+            <ImgProject src={hoveredProject.cover} />
+          </div>
+        )}
+        <div className="projects">
+          <div className={props.title}>
+            <h1 className={kelson_meduim.className}>PROJETS</h1>
+            <p className={kelson_regular.className}>3</p>
+          </div>
 
-
-        <div className={props.projects}>
-            <div className="gp-mask">
-                <div className="bg-mask" />
-                {hoveredProject && (
-                    <div className="project-cover">
-                        <ImgProject src={hoveredProject.cover} />
-                    </div>
-                )}
-                <div className="projects">
-                    <div className={props.title}>
-                        <h1 className={kelson_meduim.className}>PROJETS</h1>
-                        <p className={kelson_regular.className}>3</p>
-                    </div>
-
-                    <div className="projects-list">
-                        {Projects.map((project, e) => (
-                            <div
-                                className={props.project}
-                                key={project.id}
-                                onMouseEnter={() => handleMouseEnter(project)}
-                                onMouseLeave={handleMouseLeave}
-                            >
-                                <ProjectLigne
-                                    key={project.id}
-                                    name={project.name}
-                                    category={project.category}
-                                />
-                            </div>
-                        ))}
-                    </div>
-                </div>
-            </div>
-
+          <div className="projects-list">
+            {Projects.map((project, e) => (
+              <div
+                className={props.project}
+                key={project.id}
+                onMouseEnter={() => handleMouseEnter(project)}
+                onMouseLeave={handleMouseLeave}
+              >
+                <ProjectLigne
+                  key={project.id}
+                  id={project.id}
+                  name={project.name}
+                  category={project.category}
+                />
+              </div>
+            ))}
+          </div>
         </div>
+      </div>
+    </div>
+  );
+};
 
-    )
-}
-
-export default ProjectPage
+export default ProjectPage;
--- app/components/Project Page/Projet Ligne/ProjectLigne.jsx
+++ app/components/Project Page/Projet Ligne/ProjectLigne.jsx
@@ -1,35 +1,41 @@
-'use client'
-import React, { useState } from 'react'
-import Link from 'next/link'
-import { Projects } from '../../../data/Projects';
-import { kelson_regular, kelson, kelson_meduim } from '@/app/fonts/kelson/kelson'
+"use client";
+import React, { useState } from "react";
+import Link from "next/link";
+import { Projects } from "../../../data/Projects";
+import {
+  kelson_regular,
+  kelson,
+  kelson_meduim,
+} from "@/app/fonts/kelson/kelson";
 
-
-import './ProjectLigne.css'
+import "./ProjectLigne.css";
 
 const ProjectLigne = (props) => {
+  const [isHovered, setIsHovered] = useState(false);
 
-    const [isHovered, setIsHovered] = useState(false);
+  const handleMouseEnter = () => {
+    setIsHovered(true);
+  };
 
-    const handleMouseEnter = () => {
-        setIsHovered(true);
-    };
+  const handleMouseLeave = () => {
+    setIsHovered(false);
+  };
 
-    const handleMouseLeave = () => {
-        setIsHovered(false);
-    };
+  return (
+    <Link
+      href={{
+        pathname: "/project/[id].js", // the path to your page
+        query: { id: props.id }, // the id of the project
+      }}
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
+    >
+      <span className={isHovered ? kelson.className : kelson_meduim.className}>
+        {props.name}
+      </span>
+      <span className={kelson_regular.className}>{props.category}</span>
+    </Link>
+  );
+};
 
-
-
-    return (
-        <Link href="#" onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
-                <span className={isHovered ? kelson.className : kelson_meduim.className}>
-                    {props.name}
-                    </span>
-                <span className={kelson_regular.className}>{props.category}</span>        
-
-        </Link>
-    )
-}
-
-export default ProjectLigne
+export default ProjectLigne;
